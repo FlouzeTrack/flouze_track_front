7dfe1647f5dd1e9ca73ba78a94aabdf6fba8d511
MaximePETIT-code <contactmaximepetit@gmail.com>
2025-01-14 10:25:16 +0100

    feat: update configuration and dependencies for improved Vite integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    port: 5173,
-    open: true,
   },
 });
